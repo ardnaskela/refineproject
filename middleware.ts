f2023f71a81ace98dfbe56dfcd3a60ead07da42a
ardnaskela <alexandra.sergeyeva@gmail.com>
2023-04-06 19:58:13 +0200

    fixing excluded pages

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware"
 
 export const config = {
-    matcher: ["/((?!login|static|favicon.ico).*)"],
+    matcher: ["((?!/login|/static|/favicon.ico).*)"],
   };
